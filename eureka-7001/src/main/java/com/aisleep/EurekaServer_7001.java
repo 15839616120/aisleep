68e22fa68b799cf409abcd095d19aba73fad0376
azhe <wyzshiwu@163.com>
2019-12-29 17:46:55 +0800

    EnableEurekaServer

--- eureka-7001/src/main/java/com/aisleep/EurekaServer_7001.java
+++ eureka-7001/src/main/java/com/aisleep/EurekaServer_7001.java
@@ -4,7 +4,8 @@ package com.aisleep;
  * 启动之后访问http://localhost:7001/就可以找到服务监控页面
  */
 @SpringBootApplication
-@EnableEurekaServer //该注解表示服务端启动类，可以接受别人注册进来
+//该注解表示服务端启动类，可以接受别人注册进来
+@EnableEurekaServer
 public class EurekaServer_7001 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer_7001.class,args);
