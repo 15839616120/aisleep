a41b0c82a2292de573241ecc42a7d58f3157cc03
wyz <wuyizhe@fang.com>
2019-12-30 16:28:07 +0800

    provider-8001

--- eureka-7001/src/main/java/com/aisleep/EurekaServer_7001.java
+++ eureka-7001/src/main/java/com/aisleep/EurekaServer_7001.java
@@ -1,13 +1,19 @@
 package com.aisleep;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * 启动之后访问http://localhost:7001/就可以找到服务监控页面
+ * EnableEurekaServer该注解表示服务端启动类，可以接受别人注册进来
  */
+
 @SpringBootApplication
-//该注解表示服务端启动类，可以接受别人注册进来
 @EnableEurekaServer
 public class EurekaServer_7001 {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServer_7001.class,args);
+        SpringApplication.run(EurekaServer_7001.class, args);
     }
 }
