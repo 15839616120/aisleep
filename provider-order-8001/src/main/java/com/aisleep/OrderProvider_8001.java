13b7b4e5fd6ffb28c7aaabd294519c6041243a63
azhe <wyzshiwu@163.com>
2020-01-06 20:57:41 +0800

    注册服务

--- provider-order-8001/src/main/java/com/aisleep/OrderProvider_8001.java
+++ provider-order-8001/src/main/java/com/aisleep/OrderProvider_8001.java
@@ -1,12 +1,14 @@
 package com.aisleep;
 
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author 启动类
+ * EnableEurekaClient 该注解的作用：在服务启动以后自动注册到Eureka服务中
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderProvider_8001 {
 
     public static void main(String[] args) {
