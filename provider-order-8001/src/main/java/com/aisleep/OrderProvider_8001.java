77ed72ae7f6e7392b69541d28e166cc8b98f378f
azhe <wyzshiwu@163.com>
2020-01-06 21:42:03 +0800

    注册服务

--- provider-order-8001/src/main/java/com/aisleep/OrderProvider_8001.java
+++ provider-order-8001/src/main/java/com/aisleep/OrderProvider_8001.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author 启动类
  * EnableEurekaClient 该注解的作用：在服务启动以后自动注册到Eureka服务中
+ * EnableDiscoveryClient 服务发现的注解，可以不用配该注解
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class OrderProvider_8001 {
 
     public static void main(String[] args) {
--- provider-order-8001/src/main/java/com/aisleep/controller/OrderController.java
+++ provider-order-8001/src/main/java/com/aisleep/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.aisleep.controller;
 import com.aisleep.pojo.Order;
 import com.aisleep.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +17,8 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private DiscoveryClient client;
 
     @PostMapping("/order/addOrder")
     public boolean addOrder() {
@@ -29,5 +33,26 @@ public class OrderController {
     @GetMapping("/order/queryById")
     public List<Order> queryList() {
         return orderService.queryList();
+    }
+
+    /**
+     * 获取注册进eureka的一些服务信息
+     */
+    @GetMapping("/order/discovery")
+    public Object discovery(){
+        //获取微服务列表清单
+        List<String> server = client.getServices();
+        System.out.println(server);
+        //得到一个具体的微服务信息，可以得到该微服务的具体信息
+        List<ServiceInstance> list = client.getInstances("此处为微服务的名称");
+        for (ServiceInstance serviceInstance : list) {
+            System.out.println(
+                    serviceInstance.getHost()+"\t"+
+                    serviceInstance.getPort()+"\t"+
+                    serviceInstance.getUri()+"\t"+
+                    serviceInstance.getServiceId()+"\t"
+            );
+        }
+        return this.client;
     }
 }
